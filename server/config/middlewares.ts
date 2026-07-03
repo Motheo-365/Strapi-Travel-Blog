62564fbc49cf595f24d92a7f8501c1916dc0ded1
Motheo-365 <motheomorena6@gmail.com>
2026-07-03 16:22:34 +0200

    fixxxxxxxxx

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,7 +1,20 @@
 export default [
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://motheotravelblog.netlify.app',
+        'https://strapi-travel-blog-production.up.railway.app',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:1337',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: '*',
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
